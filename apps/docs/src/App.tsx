db6ab50b9fdd03c7cec654d4304763a3576b94f1
changsik <lowmans00@gmail.com>
2024-06-21 01:19:09 +0900

    chore: tttributify preset typeScript support

--- apps/docs/src/App.tsx
+++ apps/docs/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         <h1 className='mt-2em animate-bounce-alt animate-duration-2s'>Hello Vite + React!</h1>
         <p>
           <button
-            className='bg-blue-400 hover:bg-blue-500 text-sm text-white font-mono font-light py-2 px-4 rounded border-2 border-blue-200 dark:bg-blue-500 dark:hover:bg-blue-600'
+            className='bg-blue-400 hover:bg-blue-500 text-sm text-white font-mono font-light py-2 px-4 rounded border-2 border-blue-200 dark:bg-blue-500 dark:hover:bg-blue-600 cursor-pointer'
             type='button'
             onClick={() => setCount((count) => count + 1)}
           >
@@ -26,6 +26,7 @@ function App() {
             m='l-1em'
             border='2 rounded blue-200'
             type='button'
+            cursor='pointer'
             onClick={() => setCount((count) => count + 1)}
           >
             count is: {count}
