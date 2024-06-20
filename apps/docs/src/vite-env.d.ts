3f58afdb8da95309e1c907e6c8f4e1c33f511fa1
changsik <lowmans00@gmail.com>
2024-06-21 01:22:46 +0900

    chore: d.ts 통합

--- apps/docs/src/vite-env.d.ts
+++ apps/docs/src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+/**
+ * https://unocss.dev/presets/attributify#typescript-support-jsx-tsx
+ * shims.d.ts
+ */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import type { AttributifyAttributes } from '@unocss/preset-attributify'
+
+declare module 'react' {
+  interface HTMLAttributes<T> extends AttributifyAttributes {}
+}
