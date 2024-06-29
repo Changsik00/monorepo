4884293a79cf3d4193335097cf87aa918b77a32c
changsik <lowmans00@gmail.com>
2024-06-29 14:32:47 +0900

    chore: eslint종속성 변경

--- apps/docs/.eslintrc.cjs
+++ apps/docs/.eslintrc.cjs
@@ -7,12 +7,6 @@ module.exports = {
       files: ['src/**/*', 'vite.config.js'],
       rules: {
         // 여기에 특정 파일에 대한 규칙을 추가할 수 있습니다.
-        'react/no-unknown-property': [
-          'error',
-          {
-            ignore: ['bg', 'text', 'font', 'p', 'm', 'border', 'type', 'cursor'], // Unocss Attributify 모드 속성 무시
-          },
-        ],
       },
     },
   ],
